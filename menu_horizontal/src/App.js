6c6e361ba339f6f5ea7a05ba1a3429fa67ca5994
gFelicio <gustavo.a.felicio@gmail.com>
2021-08-03 01:30:22 +0000

    first commit

---
+++ menu_horizontal/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  	return (
+		<div>
+			<section id="inicio">
+				<h2>
+					Início
+				</h2>
+			</section>
+
+			<section id="cursos">
+				<h3>
+					Cursos
+				</h3>
+			</section>
+		</div>
+  	);
+}
+
+export default App;
+
+/**
+ * 
+ * início header menu
+ * cursos
+ * contatos
+ * parceiros
+ * missão
+ * rodapé footer
+ * 
+ */
